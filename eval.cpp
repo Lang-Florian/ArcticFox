713dc13bd25bb59441b33b1547302ad61c7779fe
neutron <n3u1r0n@proton.me>
2022-07-10 13:58:20 +0200

    template for the eval type functions

---
+++ eval.cpp
@@ -0,0 +1,22 @@
+#ifdef __INTELLISENSE__
+  #pragma diag_suppress 28
+#endif
+
+
+#include "constants.cpp"
+
+
+#ifndef __EVAL__MODULE__
+#define __EVAL__MODULE__
+
+
+/* some move definitions about evaluations
+
+*/
+
+namespace EVAL {
+
+};
+
+
+#endif

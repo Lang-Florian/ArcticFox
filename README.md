3a11b7dda1a6b9c814833645c18e4625f99ed81c
neutron <n3u1r0n@proton.me>
2022-07-11 13:16:58 +0200

    changed filenames

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 A chess engine written in C++.
 
-Compile with ```g++ main.cpp -o main -fconstexpr-ops-limit=100000000000 -std=c++23 -Ofast -march=native -flto -fno-signed-zeros -frename-registers -funroll-loops```
+Compile with ```g++ chessfox.cpp -o chessfox -fconstexpr-ops-limit=100000000000 -std=c++23 -Ofast -march=native -flto -fno-signed-zeros -frename-registers -funroll-loops```
--- chessfox.cpp
+++ chessfox.cpp
@@ -3,21 +3,77 @@
 #endif
 
 
-#include "constants.cpp"
+#include "attack_rays.cpp"
+#include "attacks.cpp"
 #include "board.cpp"
+#include "constants.cpp"
+#include "correct_attacks.cpp"
+#include "magic.cpp"
+#include "moves.cpp"
+#include "random.cpp"
+#include "time.cpp"
+#include "zobrist.cpp"
+
+#include "iostream"
 
 
-#ifndef __CHESSFOX__MODULE__
-#define __CHESSFOX__MODULE__
+using namespace std;
 
+int main() {
+  U64 time, count, depth;
+  Board board;
 
-class ChessFox {
-  public:
-    Board board;
+  board.set_fen(FEN::initial);
+  time = TIME::ms();
+  depth = 6;
+  count = board.perft(depth, true);
+  cout << "\tPerft " << depth << " of " << endl;
+  cout << "\t" << board.fen() << endl;
+  cout << "\tTotal:\t" << count << "\t\tMN/s:\t" << ((float)count) / (1000 * (float)(TIME::ms() - time)) << endl;
+  cout << endl;
 
-  ChessFox(string_t fen) {
-    board = Board(fen);
-  };
+  board.set_fen(FEN::kiwipete);
+  time = TIME::ms();
+  depth = 5;
+  count = board.perft(depth, true);
+  cout << "\tPerft " << depth << " of " << endl;
+  cout << "\t" << board.fen() << endl;
+  cout << "\tTotal:\t" << count << "\t\tMN/s:\t" << ((float)count) / (1000 * (float)(TIME::ms() - time)) << endl;
+  cout << endl;
+
+  board.set_fen(FEN::pos3);
+  time = TIME::ms();
+  depth = 7;
+  count = board.perft(depth, true);
+  cout << "\tPerft " << depth << " of " << endl;
+  cout << "\t" << board.fen() << endl;
+  cout << "\tTotal:\t" << count << "\t\tMN/s:\t" << ((float)count) / (1000 * (float)(TIME::ms() - time)) << endl;
+  cout << endl;
+
+  board.set_fen(FEN::pos4);
+  time = TIME::ms();
+  depth = 5;
+  count = board.perft(depth, true);
+  cout << "\tPerft " << depth << " of " << endl;
+  cout << "\t" << board.fen() << endl;
+  cout << "\tTotal:\t" << count << "\t\tMN/s:\t" << ((float)count) / (1000 * (float)(TIME::ms() - time)) << endl;
+  cout << endl;
+
+  board.set_fen(FEN::pos5);
+  time = TIME::ms();
+  depth = 5;
+  count = board.perft(depth, true);
+  cout << "\tPerft " << depth << " of " << endl;
+  cout << "\t" << board.fen() << endl;
+  cout << "\tTotal:\t" << count << "\t\tMN/s:\t" << ((float)count) / (1000 * (float)(TIME::ms() - time)) << endl;
+  cout << endl;
+
+  board.set_fen(FEN::pos6);
+  time = TIME::ms();
+  depth = 5;
+  count = board.perft(depth, true);
+  cout << "\tPerft " << depth << " of " << endl;
+  cout << "\t" << board.fen() << endl;
+  cout << "\tTotal:\t" << count << "\t\tMN/s:\t" << ((float)count) / (1000 * (float)(TIME::ms() - time)) << endl;
+  cout << endl;
 };
-
-#endif

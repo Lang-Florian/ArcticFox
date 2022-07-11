a0c3101fd39d39c006d17f14a370e974079852f0
neutron <n3u1r0n@proton.me>
2022-07-11 10:29:02 +0200

    fixed output formatting

--- main.cpp
+++ main.cpp
@@ -27,47 +27,53 @@ int main() {
   time = TIME::ms();
   depth = 6;
   count = board.perft(depth, true);
-  cout << "  Perft " << depth << " of " << endl << board.fen() << endl;
-  cout << "  Total:\t" << count << "\t\tMN/s:\t" << ((float)count) / (1000 * (float)(TIME::ms() - time)) << endl;
+  cout << "\tPerft " << depth << " of " << endl;
+  cout << "\t" << board.fen() << endl;
+  cout << "\tTotal:\t" << count << "\t\tMN/s:\t" << ((float)count) / (1000 * (float)(TIME::ms() - time)) << endl;
   cout << endl;
 
   board.set_fen(FEN::kiwipete);
   time = TIME::ms();
   depth = 5;
   count = board.perft(depth, true);
-  cout << "  Perft " << depth << " of " << endl << board.fen() << endl;
-  cout << "  Total:\t" << count << "\t\tMN/s:\t" << ((float)count) / (1000 * (float)(TIME::ms() - time)) << endl;
+  cout << "\tPerft " << depth << " of " << endl;
+  cout << "\t" << board.fen() << endl;
+  cout << "\tTotal:\t" << count << "\t\tMN/s:\t" << ((float)count) / (1000 * (float)(TIME::ms() - time)) << endl;
   cout << endl;
 
   board.set_fen(FEN::pos3);
   time = TIME::ms();
   depth = 7;
   count = board.perft(depth, true);
-  cout << "  Perft " << depth << " of " << endl << board.fen() << endl;
-  cout << "  Total:\t" << count << "\t\tMN/s:\t" << ((float)count) / (1000 * (float)(TIME::ms() - time)) << endl;
+  cout << "\tPerft " << depth << " of " << endl;
+  cout << "\t" << board.fen() << endl;
+  cout << "\tTotal:\t" << count << "\t\tMN/s:\t" << ((float)count) / (1000 * (float)(TIME::ms() - time)) << endl;
   cout << endl;
 
   board.set_fen(FEN::pos4);
   time = TIME::ms();
   depth = 5;
   count = board.perft(depth, true);
-  cout << "  Perft " << depth << " of " << endl << board.fen() << endl;
-  cout << "  Total:\t" << count << "\t\tMN/s:\t" << ((float)count) / (1000 * (float)(TIME::ms() - time)) << endl;
+  cout << "\tPerft " << depth << " of " << endl;
+  cout << "\t" << board.fen() << endl;
+  cout << "\tTotal:\t" << count << "\t\tMN/s:\t" << ((float)count) / (1000 * (float)(TIME::ms() - time)) << endl;
   cout << endl;
 
   board.set_fen(FEN::pos5);
   time = TIME::ms();
   depth = 4;
   count = board.perft(depth, true);
-  cout << "  Perft " << depth << " of " << endl << board.fen() << endl;
-  cout << "  Total:\t" << count << "\t\tMN/s:\t" << ((float)count) / (1000 * (float)(TIME::ms() - time)) << endl;
+  cout << "\tPerft " << depth << " of " << endl;
+  cout << "\t" << board.fen() << endl;
+  cout << "\tTotal:\t" << count << "\t\tMN/s:\t" << ((float)count) / (1000 * (float)(TIME::ms() - time)) << endl;
   cout << endl;
 
   board.set_fen(FEN::pos6);
   time = TIME::ms();
   depth = 5;
   count = board.perft(depth, true);
-  cout << "  Perft " << depth << " of " << endl << board.fen() << endl;
-  cout << "  Total:\t" << count << "\t\tMN/s:\t" << ((float)count) / (1000 * (float)(TIME::ms() - time)) << endl;
+  cout << "\tPerft " << depth << " of " << endl;
+  cout << "\t" << board.fen() << endl;
+  cout << "\tTotal:\t" << count << "\t\tMN/s:\t" << ((float)count) / (1000 * (float)(TIME::ms() - time)) << endl;
   cout << endl;
 };

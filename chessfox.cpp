1b6512fb1a7c80c6846ffafdab995e3ecbd2ba9e
neutron <n3u1r0n@proton.me>
2022-07-13 04:12:14 +0200

    implemented refactoring

--- chessfox.cpp
+++ chessfox.cpp
@@ -1,79 +1,73 @@
-#ifdef __INTELLISENSE__
-  #pragma diag_suppress 28
-#endif
-
+#include <iostream>
 
 #include "attack_rays.cpp"
 #include "attacks.cpp"
 #include "board.cpp"
 #include "constants.cpp"
-#include "correct_attacks.cpp"
 #include "magic.cpp"
-#include "moves.cpp"
 #include "random.cpp"
+#include "stack.cpp"
 #include "time.cpp"
 #include "zobrist.cpp"
 
-#include "iostream"
-
-
 using namespace std;
 
+
 int main() {
-  U64 time, count, depth;
+  u64_t time, count, depth;
   Board board;
 
-  board.set_fen(FEN::initial);
-  time = TIME::ms();
+  board.set_fen(fen::starting);
+  time = timing::ns();
   depth = 6;
   count = board.perft(depth, true);
   cout << "\tPerft " << depth << " of " << endl;
   cout << "\t" << board.fen() << endl;
-  cout << "\tTotal:\t" << count << "\t\tMN/s:\t" << ((float)count) / (1000 * (float)(TIME::ms() - time)) << endl;
+  cout << "\tTotal:\t" << count << "\t\tMN/s:\t" << 1000 * ((float)count) / ((float)(timing::ns() - time)) << endl;
   cout << endl;
 
-  board.set_fen(FEN::kiwipete);
-  time = TIME::ms();
+  board.set_fen(fen::pos2);
+  time = timing::ns();
   depth = 5;
   count = board.perft(depth, true);
   cout << "\tPerft " << depth << " of " << endl;
   cout << "\t" << board.fen() << endl;
-  cout << "\tTotal:\t" << count << "\t\tMN/s:\t" << ((float)count) / (1000 * (float)(TIME::ms() - time)) << endl;
+  cout << "\tTotal:\t" << count << "\t\tMN/s:\t" << 1000 * ((float)count) / ((float)(timing::ns() - time)) << endl;
   cout << endl;
 
-  board.set_fen(FEN::pos3);
-  time = TIME::ms();
+  board.set_fen(fen::pos3);
+  time = timing::ns();
   depth = 7;
   count = board.perft(depth, true);
   cout << "\tPerft " << depth << " of " << endl;
   cout << "\t" << board.fen() << endl;
-  cout << "\tTotal:\t" << count << "\t\tMN/s:\t" << ((float)count) / (1000 * (float)(TIME::ms() - time)) << endl;
+  cout << "\tTotal:\t" << count << "\t\tMN/s:\t" << 1000 * ((float)count) / ((float)(timing::ns() - time)) << endl;
   cout << endl;
 
-  board.set_fen(FEN::pos4);
-  time = TIME::ms();
+  board.set_fen(fen::pos4);
+  time = timing::ns();
   depth = 5;
   count = board.perft(depth, true);
   cout << "\tPerft " << depth << " of " << endl;
   cout << "\t" << board.fen() << endl;
-  cout << "\tTotal:\t" << count << "\t\tMN/s:\t" << ((float)count) / (1000 * (float)(TIME::ms() - time)) << endl;
+  cout << "\tTotal:\t" << count << "\t\tMN/s:\t" << 1000 * ((float)count) / ((float)(timing::ns() - time)) << endl;
   cout << endl;
 
-  board.set_fen(FEN::pos5);
-  time = TIME::ms();
+  board.set_fen(fen::pos5);
+  time = timing::ns();
   depth = 5;
   count = board.perft(depth, true);
   cout << "\tPerft " << depth << " of " << endl;
   cout << "\t" << board.fen() << endl;
-  cout << "\tTotal:\t" << count << "\t\tMN/s:\t" << ((float)count) / (1000 * (float)(TIME::ms() - time)) << endl;
+  cout << "\tTotal:\t" << count << "\t\tMN/s:\t" << 1000 * ((float)count) / ((float)(timing::ns() - time)) << endl;
   cout << endl;
 
-  board.set_fen(FEN::pos6);
-  time = TIME::ms();
+  board.set_fen(fen::pos6);
+  time = timing::ns();
   depth = 5;
   count = board.perft(depth, true);
   cout << "\tPerft " << depth << " of " << endl;
   cout << "\t" << board.fen() << endl;
-  cout << "\tTotal:\t" << count << "\t\tMN/s:\t" << ((float)count) / (1000 * (float)(TIME::ms() - time)) << endl;
+  cout << "\tTotal:\t" << count << "\t\tMN/s:\t" << 1000 * ((float)count) / ((float)(timing::ns() - time)) << endl;
   cout << endl;
 };

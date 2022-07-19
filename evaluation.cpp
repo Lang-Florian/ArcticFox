ddbf2ad86ca195d1e5dfe4acdc4577757d5adeed
neutron <n3u1r0n@proton.me>
2022-07-20 01:15:26 +0200

    added eval file

---
+++ evaluation.cpp
@@ -0,0 +1,21 @@
+#ifdef __INTELLISENSE__
+  #pragma diag_suppress 28
+#endif
+
+
+#ifndef __EVALUATION__MODULE__
+#define __EVALUATION__MODULE__
+
+
+#include <string>
+
+#include "constants.cpp"
+#include "types.cpp"
+
+
+namespace eval {
+  
+};
+
+
+#endif // __EVALUATION__MODULE__

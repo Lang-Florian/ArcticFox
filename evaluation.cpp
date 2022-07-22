ea9fcf16ad7ebe03f9b2be5e8f20131da6b682ea
neutron <n3u1r0n@proton.me>
2022-07-22 15:39:00 +0200

    added value array

--- evaluation.cpp
+++ evaluation.cpp
@@ -20,7 +20,7 @@ namespace eval {
     draw = 0,
   };
 
-  constexpr eval_t positional_pawn_value[64] = {
+  constexpr eval_t pawn_value[64] = {
       0,   0,   0,   0,   0,   0,   0,   0,
     150, 150, 150, 150, 150, 150, 150, 150,
     110, 110, 120, 130, 130, 120, 110, 110,
@@ -31,7 +31,7 @@ namespace eval {
       0,   0,   0,   0,   0,   0,   0,   0,
   };
 
-  constexpr eval_t positional_knight_value[64] = {
+  constexpr eval_t knight_value[64] = {
     300, 310, 320, 320, 320, 320, 310, 300,
     310, 330, 350, 350, 350, 350, 330, 310,
     320, 350, 360, 365, 365, 360, 350, 320,
@@ -42,7 +42,7 @@ namespace eval {
     300, 310, 320, 320, 320, 320, 310, 300,
   };
 
-  constexpr eval_t positional_bishop_value[64] = {
+  constexpr eval_t bishop_value[64] = {
     330, 340, 340, 340, 340, 340, 340, 330,
     340, 350, 350, 350, 350, 350, 350, 340,
     340, 350, 355, 360, 360, 355, 350, 340,
@@ -53,7 +53,7 @@ namespace eval {
     330, 340, 340, 340, 340, 340, 340, 330,
   };
 
-  constexpr eval_t positional_rook_value[64] = {
+  constexpr eval_t rook_value[64] = {
     525, 525, 525, 525, 525, 525, 525, 525,
     530, 535, 535, 535, 535, 535, 535, 530,
     520, 525, 525, 525, 525, 525, 525, 520,
@@ -64,7 +64,7 @@ namespace eval {
     525, 525, 525, 530, 530, 525, 525, 525,
   };
 
-  constexpr eval_t positional_queen_value[64] = {
+  constexpr eval_t queen_value[64] = {
      980,  990,  990,  995,  995,  990,  990,  980,
      990, 1000, 1000, 1000, 1000, 1000, 1000,  990,
      990, 1000, 1005, 1005, 1005, 1005, 1000,  990,
@@ -75,7 +75,7 @@ namespace eval {
      980,  990,  990,  995,  995,  990,  990,  980,
   };
 
-  constexpr eval_t positional_king_value_middlegame[64] = {
+  constexpr eval_t king_value_middlegame[64] = {
     -30, -40, -40, -50, -50, -40, -40, -30,
     -30, -40, -40, -50, -50, -40, -40, -30,
     -30, -40, -40, -50, -50, -40, -40, -30,
@@ -86,7 +86,7 @@ namespace eval {
      20,  30,  10,   0,   0,  10,  30,  20,
   };
 
-  constexpr eval_t positional_king_value_endgame[64] = {
+  constexpr eval_t king_value_endgame[64] = {
     -50, -40, -30, -20, -20, -30, -40, -50,
     -30, -20, -10,   0,   0, -10, -20, -30,
     -30, -10,  20,  30,  30,  20, -10, -30,
@@ -96,7 +96,48 @@ namespace eval {
     -30, -30,   0,   0,   0,   0, -30, -30,
     -50, -30, -30, -30, -30, -30, -30, -50,
   };
+
+  template <piece_t piece>
+  eval_t value(square_t square, float endgame_factor=0) {
+    if constexpr (piece == piece::white_pawn) {
+      return pawn_value[square];
+    } else if constexpr (piece == piece::black_pawn) {
+      return pawn_value[square ^ 56];
+    } else if constexpr (piece == piece::white_knight) {
+      return knight_value[square];
+    } else if constexpr (piece == piece::black_knight) {
+      return knight_value[square ^ 56];
+    } else if constexpr (piece == piece::white_bishop) {
+      return bishop_value[square];
+    } else if constexpr (piece == piece::black_bishop) {
+      return bishop_value[square ^ 56];
+    } else if constexpr (piece == piece::white_rook) {
+      return rook_value[square];
+    } else if constexpr (piece == piece::black_rook) {
+      return rook_value[square ^ 56];
+    } else if constexpr (piece == piece::white_queen) {
+      return queen_value[square];
+    } else if constexpr (piece == piece::black_queen) {
+      return queen_value[square ^ 56];
+    } else if constexpr (piece == piece::white_king) {
+      return endgame_factor * eval::king_value_endgame[square] +
+             (1 - endgame_factor) * eval::king_value_middlegame[square];
+    } else if constexpr (piece == piece::black_king) {
+      return endgame_factor * eval::king_value_endgame[square ^ 56] +
+             (1 - endgame_factor) * eval::king_value_middlegame[square ^ 56];
+    }
+    return 0;
+  };
+
+  constexpr eval_t (*(value_array[32]))(square_t, float) = {
+     value<0>,  value<1>,  value<2>,  value<3>,  value<4>,  value<5>,  value<6>,  value<7>,
+     value<8>,  value<9>, value<10>, value<11>, value<12>, value<13>, value<14>, value<15>,
+    value<16>, value<17>, value<18>, value<19>, value<20>, value<21>, value<22>, value<23>,
+    value<24>, value<25>, value<26>, value<27>, value<28>, value<29>, value<30>, value<31>,
+  };
 };
+
+
 
 
 #endif // __EVALUATION__MODULE__

73ded76d1de18b56c95156110ee733d3f7b146bb
neutron <n3u1r0n@proton.me>
2022-07-22 15:39:18 +0200

    primitive hash table setup

--- hashtable.cpp
+++ hashtable.cpp
@@ -7,23 +7,37 @@
 #define __HASHTABLE__MODULE__
 
 
+#include <array>
+
 #include "constants.cpp"
 #include "types.cpp"
 
 
 struct HashTableEntry {
   hash_t hash;
+  eval_t eval;
+  move_t move;
+  u8_t depth;
+  u8_t flags;
 };
 
 
+template <int size>
 class HashTable {
   private:
-    HashTableEntry table[HASH_TABLE_SIZE];
-    hash_t mask = HASH_TABLE_SIZE - 1;
+    std::array<HashTableEntry, size> table;
+    hash_t mask = size - 1;
 
-  public:
-
+  public:    
+    HashTableEntry& get(hash_t hash) {
+      return table[hash & mask];
+    };
 };
+
+
+constexpr u8_t exact_bound = 0;
+constexpr u8_t lower_bound = 1;
+constexpr u8_t upper_bound = 2;
 
 
 #endif // __HASHTABLE__MODULE__

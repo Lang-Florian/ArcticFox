5be20aebcda617ed8720907c390852b9c5821e57
n3u1r0n <n3u1r0n@pm.me>
2022-09-09 19:28:49 +0200

    removed double and single checks

--- base/movetype.cpp
+++ base/movetype.cpp
@@ -10,13 +10,11 @@
 
 namespace movetype {
 enum : movetype_t {
-  quiet =        0b0001,
-  single_check = 0b0010,
-  double_check = 0b0100,
-  check =        0b0110,
-  capture =      0b1000,
-  non_quiet =    0b1110,
-  legal =        0b1111,
+  quiet =     0b001,
+  check =     0b010,
+  capture =   0b100,
+  non_quiet = 0b110,
+  legal =     0b111,
 };
 };
 
